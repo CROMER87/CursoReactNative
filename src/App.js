2bf02fcfc0d22eaf71595aa688ab3c2badb8fdbe
Romero Jose Ferreira <romero@bb.com.br>
2022-01-13 09:27:53 -0300

    Fisrt Commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+function App() {
+  
+  return (
+  <section> 
+      <form>
+        <input type="text" placeholder="Título" />
+        <textarea placeholder="Escreva sua Nota.."></textarea>
+        <button>Criar Nota</button>
+      </form>
+      <ul>
+        <li>
+          <section>
+            <header>
+              <h3>
+                Título
+              </h3>
+            </header>
+            <p>Escreva Sua Nota</p>
+          </section>
+        </li>
+      </ul>
+
+
+  </section>
+  );
+     
+}
+
+export default App;
